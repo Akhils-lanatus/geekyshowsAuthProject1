a1b92e07013398d64ba4c7653c4f3ddd31c157a8
Akhil Shah <akhil.s@lanatussystems.com>
2024-06-09 21:56:20 +0530

    Backend completed

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import { User } from "../model/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
+import { transporter } from "../config/emailConfig.js";
 const GetUser = async (req, res) => {
   try {
     const user = req.user;
@@ -175,4 +175,104 @@ const ChangePassword = async (req, res) => {
   }
 };
 
-export { RegisterUser, LoginUser, GetUser, ChangePassword };
+const SendResetPasswordEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (!email?.trim()) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter an email",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Email does not exist.",
+      });
+    }
+
+    const secret = user._id + process.env.JSON_WEB_TOKEN;
+    const token = jwt.sign({ id: user._id }, secret, { expiresIn: "15m" });
+
+    const link = `http://localhost:3000/api/v1/auth/reset-password/${user._id}/${token}`;
+
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_FROM,
+      to: user.email,
+      subject: "UserAuth - Reset Password",
+      html: `<a href=${link}>Click Here</a> To Reset Password`,
+    });
+    return res
+      .status(200)
+      .json({ success: true, message: "Password reset link sent ", info });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const ResetUserPassword = async (req, res) => {
+  try {
+    const { password } = req.body;
+    if (!password) {
+      return res.status(400).json({
+        success: false,
+        message: "Enter Password",
+      });
+    }
+    const { id, token } = req.params;
+    if (!id || !token) {
+      return res.status(400).json({
+        success: false,
+        message: "Something Went Wrong, Missing Data",
+      });
+    }
+
+    const user = await User.findById(id);
+    const new_secret = user._id + process.env.JSON_WEB_TOKEN;
+
+    const verifiedtoken = jwt.verify(token, new_secret);
+
+    if (verifiedtoken.exp && Date.now() >= verifiedtoken.exp * 1000) {
+      return res.status(400).json({
+        success: false,
+        message: "Token has expired",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    await User.findByIdAndUpdate(
+      id,
+      { $set: { password: hashedPass } },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Password Reset Successfull",
+    });
+  } catch (error) {
+    let tokenError = error.name === "TokenExpiredError" ? error.name : "";
+    return res.status(tokenError !== "" ? 400 : 500).json({
+      success: false,
+      message:
+        tokenError !== "" ? "Token has expired" : "Error while verifying token",
+    });
+  }
+};
+
+export {
+  RegisterUser,
+  LoginUser,
+  GetUser,
+  ChangePassword,
+  SendResetPasswordEmail,
+  ResetUserPassword,
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,8 @@ import {
   GetUser,
   LoginUser,
   RegisterUser,
+  ResetUserPassword,
+  SendResetPasswordEmail,
 } from "../controllers/user.controllers.js";
 
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -21,4 +23,11 @@ router.get("/get-user", verifyJWT, GetUser);
 
 //CHANGE-PASSWORD || PATCH
 router.patch("/change-password", verifyJWT, ChangePassword);
+
+//RESET-PASSWORD-LINK || POST
+router.post("/reset-password-link", SendResetPasswordEmail);
+
+//RESET-PASSWORD || POST
+router.post("/reset-password/:id/:token", ResetUserPassword);
+
 export default router;
